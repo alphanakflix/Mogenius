ffeb89c7550d90546db17139b4db593a79a3c840
ɴᴀᴋ ᵏᵉ <93425656+alphanakflix@users.noreply.github.com>
2023-01-30 13:07:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'GreyMatters_Bot'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://GITHUB-USERNAME:token@github.com/GITHUB-USERNAME/YOUR-REPO-NAME okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://alphanakflix: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@github.com/alphanakflix/netflix_tvshowsbot okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
